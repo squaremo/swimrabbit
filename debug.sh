c38557950b230b7115f0c9c791daeea292917e2c
Michael Bridgen <mikeb@lshift.net>
2010-02-10 17:52:29 +0000

    Use rlwrap to make rhino shell more pleasant

--- debug.sh
+++ debug.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 FILE=
 [ -n "$1" ] && FILE="-f $1"
-java -cp .:js.jar:rabbitmq-client.jar:commons-io-1.2.jar org.mozilla.javascript.tools.shell.Main $FILE
+[ -n "$1" ] || RLWRAP="rlwrap"
+$RLWRAP java -cp .:js.jar:rabbitmq-client.jar:commons-io-1.2.jar org.mozilla.javascript.tools.shell.Main $FILE
